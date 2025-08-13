549a877ebc930754e4168c3ab92af648c7cc0df5
mrdaly748 <hameddali48@gmail.com>
2025-08-13 19:02:28 +0100

    Final Project

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-col justify-center">
           <p className="text-center md:text-end">
-            © {new Date().getFullYear()} Adrian Hajdin. All rights reserved.
+            © {new Date().getFullYear()} Hamed Daly. All rights reserved.
           </p>
         </div>
       </div>
